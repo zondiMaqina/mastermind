8cb5410ffdbba66827c46aa41f09fcd1b9aa2132
zondimaqina <zondimaqina0@gmail.com>
2024-05-07 13:54:10 +0200

    initialize modules and classes

---
+++ mastermind.rb
@@ -0,0 +1,20 @@
+# module for player to use to crack code made by computor or visa versa
+module CrackCode
+  def rules
+    
+  end
+end
+
+# computor to crack code or create it 
+class Computor
+  def initialize
+    
+  end
+end
+
+# Player to crack code or create it 
+class Player
+  def initialize
+    
+  end
+end
